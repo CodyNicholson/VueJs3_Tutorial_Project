14ab770edb49dec04931486dd7d4adddc53106c9
Cody Nicholson <codynicholson96@gmail.com>
2020-09-11 19:20:53 -0500

    Added docker postgres and restructred a bit

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -13,26 +13,43 @@ app.component('product-display', {
           <img :src="image" />
         </div>
         <div class="product-info">
+
           <h1>{{ productName }}</h1>
-          <p v-if="inStock">In Stock</p>
+
+          <p v-if="inStock">In Stock: {{ amountInStock }}</p>
           <p v-else>Out of Stock</p>
+
           <p>Shipping: {{ shipping }}</p>
+
           <ul>
             <li v-for="detail in details">{{ detail }}</li>
           </ul>
-          <div class="color-circle"
-            v-for="(variant, index) in variants" 
-            :key="variant.id"
-            :style="{ backgroundColor: variant.color }"
-            @mouseover="updateProduct(index)"
-            >
-          </div> 
-          <button class="button" v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }"
-            >
-          Add to cart
-          </button>
+          
+          <div class="display-flex">
+            <p class="color-text">Colors:</p>
+            <div class="color-circle"
+                v-for="(variant, index) in variants" 
+                :key="variant.id"
+                :style="{ backgroundColor: variant.color }"
+                @mouseover="updateProduct(index)" 
+            />
+          </div>
+
+          <div class="display-flex">
+            <button class="button" v-on:click="addToCart" 
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+                >
+            Add to cart
+            </button>
+            <button class="button" v-on:click="removeFromCart" 
+                :disabled="!displayRemoveFromCart"
+                :class="{ disabledButton: !displayRemoveFromCart }"
+                >
+            Remove from cart
+            </button>
+          </div>
+
         </div>
       </div>
       <review-list :reviews="reviews"></review-list>
@@ -48,13 +65,15 @@ app.component('product-display', {
                     id: 2234,
                     color: 'green',
                     image: './assets/images/socks_green.jpg',
-                    quantity: 10
+                    quantity: 10,
+                    quantityInCart: 0
                 },
                 {
                     id: 2235,
                     color: 'blue',
                     image: './assets/images/socks_blue.jpg',
-                    quantity: 0
+                    quantity: 1,
+                    quantityInCart: 0
                 }
             ],
             reviews: [],
@@ -64,30 +83,45 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+            if (this.variants[this.selectedVariant].quantity >= this.variants[this.selectedVariant].quantityInCart) {
+                this.variants[this.selectedVariant].quantityInCart += 1;
+                this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
+            }
+        },
+        removeFromCart() {
+            if (this.variants[this.selectedVariant].quantityInCart > 0) {
+                this.variants[this.selectedVariant].quantityInCart -= 1;
+                this.$emit('remove-from-cart', this.variants[this.selectedVariant].id);
+            }
         },
         updateProduct(index) {
-            this.selectedVariant = index
+            this.selectedVariant = index;
         },
         addReview(review) {
-            this.reviews.push(review)
+            this.reviews.push(review);
         }
     },
     computed: {
         productName() {
-            return this.brand + ' ' + this.product
+            return this.brand + ' ' + this.product;
         },
         image() {
-            return this.variants[this.selectedVariant].image
+            return this.variants[this.selectedVariant].image;
         },
         inStock() {
-            return this.variants[this.selectedVariant].quantity
+            return this.variants[this.selectedVariant].quantity > this.variants[this.selectedVariant].quantityInCart;
+        },
+        amountInStock() {
+            return this.variants[this.selectedVariant].quantity - this.variants[this.selectedVariant].quantityInCart;
+        },
+        displayRemoveFromCart() {
+            return this.variants[this.selectedVariant].quantityInCart > 0;
         },
         shipping() {
             if (this.premium) {
-                return 'Free'
+                return 'Free';
             }
-            return 2.99
+            return 2.99;
         }
     }
 });
--- main.js
+++ main.js
@@ -7,7 +7,16 @@ const app = Vue.createApp({
     },
     methods: {
         updateCart(id) {
-            this.cart.push(id)
+            this.cart.push(id);
+        },
+        removeFromCart(id) {
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    console.log("Remove: " + this.cart[i]);
+                    this.cart.splice(i, 1);
+                    console.log(this.cart);
+                }
+            }
         }
     }
 });
